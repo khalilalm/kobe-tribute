f8ef39b0eb125756a56a231611b0d9b53c5ee822
Khalil Al-Mujeeb <almujeebkhalil@gmail.com>
2020-03-09 21:14:47 -0400

    Made Navbar and testing git repository system

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class NavBar extends Component {
+    
+    render() { 
+        return ( <nav><ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Background</li>
+            <li>Career</li>
+
+            </ul></nav> );
+    }
+}
+ 
+export default NavBar;
